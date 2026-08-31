2074eec7850e3eb18e4bc48ef413a4ae4f23fe1e
Royal Patel <108641849+RoyalPatel95@users.noreply.github.com>
2026-08-31 21:49:41 +0530

    Refactor AWS instance and security group setup
    Refactor AWS instance and security group configuration to use for_each for instances and dynamic ingress rules for ports.

--- terraform_basic/main.tf
+++ terraform_basic/main.tf
@@ -1,13 +1,15 @@
 resource "aws_instance" "myinstance" {
+  for_each = var.instances
   ami                         = var.ami
-  instance_type               = var.instance_type
+  instance_type               = each.value.instance_type
   key_name                    = aws_key_pair.mykey.key_name
   associate_public_ip_address = true
-
+  # for 3 instance
+  # count = 3
   vpc_security_group_ids = [
     aws_security_group.mysecurity.id
   ]
-  
+
   user_data = file("install-nginx.sh")
 
   root_block_device {
@@ -16,7 +18,7 @@ resource "aws_instance" "myinstance" {
   }
 
   tags = {
-    name = "myinstance"
+    name = each.key
   }
 }
 
@@ -26,27 +28,40 @@ resource "aws_key_pair" "mykey" {
 }
 
 resource "aws_security_group" "mysecurity" {
-  ingress {
-    description = "SSH"
-    to_port     = 22
-    from_port   = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+
+  dynamic "ingress" {
+
+    for_each = [22,80,443,8080]
+    iterator = port
+    content {
+      to_port     = port.value
+      from_port   = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
-  ingress {
-    description = "HTTP"
-    to_port     = 80
-    from_port   = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTPS"
-    to_port     = 443
-    from_port   = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
+  # ingress {
+  #   description = "SSH"
+  #   to_port     = 22
+  #   from_port   = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+  # ingress {
+  #   description = "HTTP"
+  #   to_port     = 80
+  #   from_port   = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+  # ingress {
+  #   description = "HTTPS"
+  #   to_port     = 443
+  #   from_port   = 443
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   egress {
     to_port     = "0"
