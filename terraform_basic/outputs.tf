ad3c4d533bfc891ca6841137f8b9baaca0a31145
Royal Patel <108641849+RoyalPatel95@users.noreply.github.com>
2026-08-31 21:49:25 +0530

    Refactor outputs for multiple AWS instances

--- terraform_basic/outputs.tf
+++ terraform_basic/outputs.tf
@@ -1,11 +1,35 @@
-output "instance_id" {
-  value = aws_instance.myinstance.id
+# output "instance_id" {
+#   # value = aws_instance.myinstance.id
+#   value = aws_instance.myinstance[*].id
+# }
+
+# output "instance_public_ip" {
+#   # value = aws_instance.myinstance.public_ip
+#   value = aws_instance.myinstance[*].public_ip
+# }
+
+# output "instance_private_ip" {
+#   # value = aws_instance.myinstance.private_ip
+#   value = aws_instance.myinstance[*].private_ip
+# }
+
+output "instance_ids" {
+  value = {
+    for key, instance in aws_instance.myinstance :
+    key => instance.id
+  }
 }
 
-output "instance_public_ip" {
-  value = aws_instance.myinstance.public_ip
+output "instance_public_ips" {
+  value = {
+    for key, instance in aws_instance.myinstance :
+    key => instance.public_ip
+  }
 }
 
-output "instance_private_ip" {
-  value = aws_instance.myinstance.private_ip
+output "instance_private_ips" {
+  value = {
+    for key, instance in aws_instance.myinstance :
+    key => instance.private_ip
+  }
 }
